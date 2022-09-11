4864647469b2ff893e7b8963ae03d600b4555126
Maxim Trofimov <maks.trofimov.0303@mail.ru>
2022-09-11 23:32:43 +0300

    fix random

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ class Model:
         if model_path is not None:
             self.data = self.load(model_path)
         else:
-            self.data = defaultdict(list)
+            self.data = defaultdict(dict)
 
     def fit(self, text: str):
         preproc_tokens = list(
@@ -20,8 +20,15 @@ class Model:
                 str.maketrans(string.punctuation, ' ' * len(string.punctuation))
             ).split())
         )
+
+        # сейчас префикс из 1 эл-та, тк думаю если для нескольких, то стоит приводить слова
+        # в начальную форму, чтобы лучше хранились, но сейчас использовать специальные библиотеки нельзя
+
         for i_word in range(len(preproc_tokens) - 1):
-            self.data[preproc_tokens[i_word]].append(preproc_tokens[i_word + 1])
+            if preproc_tokens[i_word + 1] in self.data[preproc_tokens[i_word]]:
+                self.data[preproc_tokens[i_word]][preproc_tokens[i_word + 1]] += 1
+            else:
+                self.data[preproc_tokens[i_word]][preproc_tokens[i_word + 1]] = 1
 
     def generate(self, length: int, first: str = None):
         keys = list(self.data.keys())
@@ -31,7 +38,13 @@ class Model:
 
         generated = [first]  # type: list[str]
         for _ in range(length):
-            predict = np.random.choice(self.data.get(generated[-1], [np.random.choice(keys)]))
+            words, counts = list(zip(*self.data.get(generated[-1], {np.random.choice(keys): 1}).items()))
+            s_counts = sum(counts)
+            p = [x / s_counts for x in counts]
+            predict = np.random.choice(
+                words,
+                p=p
+            )
             generated.append(predict)
 
         return ' '.join(generated)
